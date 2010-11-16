9d73eaf7c7f552e7bd14f13260d6469d82927de0
Evgeny Mandrikov <mandrikov@gmail.com>
2010-11-16 22:56:11 +0000

    TRAC: Migrate to parent-7

--- src/main/java/org/sonar/plugins/trac/TracMetrics.java
+++ src/main/java/org/sonar/plugins/trac/TracMetrics.java
@@ -1,5 +1,7 @@
-/**
- * Copyright (c) 2010 Thales Optronics Ltd
+/*
+ * Sonar Trac Plugin
+ * Copyright (C) 2010 Thales Optronics Ltd.
+ * dev@sonar.codehaus.org
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -7,10 +9,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
-
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
-
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,6 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package org.sonar.plugins.trac;
 
 import java.util.Arrays;
